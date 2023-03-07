375bc6599e152b7d3a4ba69f3ebbe60e1274b0f2
Annd <ssandreia30@gmail.com>
2023-03-07 11:13:13 -0300

    Exercise complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+  if (int1 <= -1 || int2 <= -1) { 
+    return "ERROR"
+  } else if(!Number.isInteger(int1) || !Number.isInteger(int2)) {
+    return "ERROR"
+  } else if( int1 > int2) {
+    let int = int1;
+    int1 = int2;
+    int2 = int;
+  }
+
+  let finalSum = 0;
+ 
+    for(int1 = 0; int1 <= int2; int1++) {
+      finalSum += int1;
+    } 
+
+  return finalSum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;

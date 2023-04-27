25974ad8fcc3339a94775c89cdd92b456a6ffa67
Annd <ssandreia30@gmail.com>
2023-04-27 10:52:04 -0300

    Complete ✓

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	 return array.reduce((a, b) => a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+ return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if(num === 0 || num === 1)
+    return 1;
+
+  while(num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
